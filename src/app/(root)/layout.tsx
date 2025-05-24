85203cb4fa0d8bcda21ac79bb0e5430420d34413
Henrik Nilsson Harnert <hello@henriknh.com>
2025-05-24 20:28:38 +0200

    Changes

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="w-full min-h-screen flex flex-col">
       <TopNav />
       <main className="flex-1">{children}</main>
     </div>
