a91985f36ae58abc868ef6c949929ca1a046b434
Henrik Nilsson Harnert <hello@henriknh.com>
2025-06-13 11:52:35 +0200

    Changes

--- src/components/root/TopNav.tsx
+++ src/components/root/TopNav.tsx
@@ -1,10 +1,17 @@
-import { Button } from "@/components/ui/button";
-import config from "@/config";
-import { getUserFromToken } from "@/lib/user";
-import Link from "next/link";
+"use client";
 
-export async function TopNav() {
-  const user = await getUserFromToken();
+import config from "@/config";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Button } from "../ui/button";
+import { CleanedUser, getUserFromToken } from "@/lib/user";
+
+export function TopNav() {
+  const [user, setUser] = useState<CleanedUser | null>(null);
+
+  useEffect(() => {
+    (async () => setUser(await getUserFromToken()))();
+  }, []);
 
   return (
     <nav className="border-b w-full">
@@ -13,21 +20,34 @@ export async function TopNav() {
           {config.appName}
         </Link>
 
-        <div className="flex items-center gap-4">
-          {user ? (
+        <div className="relative flex items-center gap-4">
+          <div
+            className="absolute top-0 bottom-0 flex items-center gap-4 transition-all duration-300"
+            style={{
+              right: user ? 0 : "-200px",
+              opacity: user ? 1 : 0,
+            }}
+          >
             <Button asChild>
               <Link href="/dashboard">Dashboard</Link>
             </Button>
-          ) : (
-            <>
-              <Button asChild variant="ghost">
-                <Link href="/auth/login">Login</Link>
-              </Button>
-              <Button asChild>
-                <Link href="/auth/register">Register</Link>
-              </Button>
-            </>
-          )}
+          </div>
+
+          <div
+            className="absolute top-0 bottom-0 flex items-center gap-4 transition-all ease-out duration-300"
+            style={{
+              opacity: user ? 0.5 : 1,
+              right: user ? "-200px" : 0,
+              scale: user ? 0.5 : 1,
+            }}
+          >
+            <Button asChild variant="ghost">
+              <Link href="/auth/login">Login</Link>
+            </Button>
+            <Button asChild>
+              <Link href="/auth/register">Register</Link>
+            </Button>
+          </div>
         </div>
       </div>
     </nav>
