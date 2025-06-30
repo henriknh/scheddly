c5f023e24bc9cee209bff6f3dbc32624f23d52c3
Henrik Nilsson Harnert <hello@henriknh.com>
2025-06-30 14:07:30 +0200

    Changes

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,4 +1,7 @@
+import config from "@/config";
 import { getUserFromToken } from "@/lib/user";
+import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function AuthLayout({
@@ -14,6 +17,12 @@ export default async function AuthLayout({
 
   return (
     <div className="min-h-screen w-full flex flex-col">
+      <div className="flex h-16 items-center justify-between w-full px-8">
+        <Link href="/" className="text-xl font-bold flex gap-2 items-center">
+          <Image src="/logo.svg" alt="Logo" width={20} height={20} />
+          {config.appName}
+        </Link>
+      </div>
       <div className="flex-1 flex items-center justify-center">{children}</div>
     </div>
   );
