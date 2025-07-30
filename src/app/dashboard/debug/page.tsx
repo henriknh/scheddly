9a7af7f09ef4975aed573ddfd86a5f5028898fa1
Henrik Nilsson Harnert <hello@henriknh.com>
2025-07-30 14:34:59 +0200

    Changes

--- src/app/dashboard/debug/page.tsx
+++ src/app/dashboard/debug/page.tsx
@@ -1,11 +1,11 @@
 import { PostPostsDebugButton } from "@/components/debug/post-posts-debug-button";
 import { ArchivePostsDebugButton } from "@/components/debug/archive-posts-debug-button";
 
-if (!process.env.CRON_SECRET) {
-  throw new Error("CRON_SECRET is not set");
-}
-
 export default function DebugPage() {
+  if (!process.env.CRON_SECRET) {
+    throw new Error("CRON_SECRET is not set");
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <PostPostsDebugButton secret={process.env.CRON_SECRET} />
