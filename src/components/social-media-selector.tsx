4fb77292034b9b74d0cdc78d7270f2357d68f655
Henrik Nilsson Harnert <hello@henriknh.com>
2025-08-11 01:30:21 +0200

    Changes

--- src/components/social-media-selector.tsx
+++ src/components/social-media-selector.tsx
@@ -96,7 +96,7 @@ export function SocialMediaSelector({
               </Button> */}
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
               {integrationsByBrand.map((integration) => {
                 return (
                   <SocialMediaSelectorCard
