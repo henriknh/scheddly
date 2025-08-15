a2871d48f6204f07eb560f1a77fe2ecf4a6c7948
Henrik Nilsson Harnert <hello@henriknh.com>
2025-08-15 12:46:05 +0200

    Changes

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,38 +1,42 @@
-import * as React from "react"
+import * as React from "react";
 
-const MOBILE_BREAKPOINT = 768
+const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const checkIsMobile = () => {
+    // Check screen width
+    const isSmallScreen = window.innerWidth < MOBILE_BREAKPOINT;
+
+    // Check for touch capabilities
+    const hasTouchScreen =
+      "ontouchstart" in window || navigator.maxTouchPoints > 0;
+
+    // Check user agent for mobile devices
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isMobileUserAgent =
+      /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(
+        userAgent
+      );
+
+    // Consider it mobile if any of these conditions are true
+    return isSmallScreen || (hasTouchScreen && isMobileUserAgent);
+  };
+
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
+    checkIsMobile()
+  );
 
   React.useEffect(() => {
-    const checkIsMobile = () => {
-      // Check screen width
-      const isSmallScreen = window.innerWidth < MOBILE_BREAKPOINT;
-      
-      // Check for touch capabilities
-      const hasTouchScreen = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
-      
-      // Check user agent for mobile devices
-      const userAgent = navigator.userAgent.toLowerCase();
-      const isMobileUserAgent = /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent);
-      
-      // Consider it mobile if any of these conditions are true
-      const mobile = isSmallScreen || (hasTouchScreen && isMobileUserAgent);
-      
-      setIsMobile(mobile);
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+    const onChange = () => {
+      setIsMobile(checkIsMobile());
     };
 
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      checkIsMobile();
-    }
-    
     mql.addEventListener("change", onChange);
-    checkIsMobile();
-    
-    return () => mql.removeEventListener("change", onChange);
-  }, [])
+    setIsMobile(checkIsMobile());
 
-  return !!isMobile
+    return () => mql.removeEventListener("change", onChange);
+  }, []);
+
+  return isMobile;
 }
