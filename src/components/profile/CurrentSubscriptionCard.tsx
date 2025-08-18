feb963733fbbdeda5a37780122768293df88c7e7
Henrik Nilsson Harnert <hello@henriknh.com>
2025-08-18 23:32:23 +0200

    Changes

--- src/components/profile/CurrentSubscriptionCard.tsx
+++ src/components/profile/CurrentSubscriptionCard.tsx
@@ -164,8 +164,8 @@ export function CurrentSubscriptionCard() {
       <CardContent>
         <Separator className="mb-4" />
 
-        <div className="flex items-center justify-between gap-2">
-          <div className="flex gap-2">
+        <div className="flex flex-col md:flex-row justify-between gap-2">
+          <div className="flex flex-col ">
             {hasActiveSubscription && (
               <>
                 <Button variant="outline" onClick={openStripePortal}>
